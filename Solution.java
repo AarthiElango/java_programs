c9dc68c610fa4bd51b6d4c040f2a5cabb0207a3a
AarthiElango <142249472+AarthiElango@users.noreply.github.com>
2023-08-26 22:19:18 +0530

    TransposeMatrix.java

--- Solution.java
+++ Solution.java
@@ -18,10 +18,10 @@ class Solution {
 
     public static int[][] transpose(int[][] ar) {
        
-       int a[][]=new [ar.length][ar[0].length];  
+       int a[][]=new int[ar[0].length][ar.length];  
         for(int i=0;i<ar.length;i++){
-            for(int j=0;j<ar.length;j++){
-                    a[i][j]=ar[j][i];
+            for(int j=0;j<ar[0].length;j++){
+                    a[j][i]=ar[i][j];
             }
         }
       
@@ -29,34 +29,3 @@ class Solution {
 
     }
 }
-
-
-
-/*
-
-import java.util.*;
-class Solution {
-    public static void main(String[] args){
-         Scanner sc =new Scanner(System.in);
-        int m=sc.nextInt();
-        int n=sc.nextInt();
-        int[][] arr=new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                    arr[i][j]=sc.nextInt();
-
-            }
-        }
-
-         int a[][]=new int[m][n];  
-        for(int i=0;i<ar.length;i++){
-            for(int j=0;j<ar.length;j++){
-                    a[i][j]=ar[j][i];
-            }
-        }
-      
-  System.out.println(Arrays.toString(a));
-       
-    }
-}
-*/
